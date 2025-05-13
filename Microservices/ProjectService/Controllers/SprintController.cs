27104e26334da10567f7fc381d86a69d1514f03d
holo2k <nneketaa@yandex.ru>
2025-05-13 20:35:01 +0500

    feat: sprint controller

--- Microservices/ProjectService/Controllers/SprintController.cs
+++ Microservices/ProjectService/Controllers/SprintController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectService.BusinessLayer.Abstractions;
 using ProjectService.Models;
 
 namespace ProjectService.Controllers
@@ -7,40 +8,66 @@ namespace ProjectService.Controllers
     [Route("sprint")]
     public class SprintController : ControllerBase
     {
-        [HttpGet("get")]
-        public async Task<IActionResult> Get()
+        private readonly ISprintManager sprintManager;
+        public SprintController(ISprintManager sprintManager)
         {
-            return Ok();
+            this.sprintManager = sprintManager;
         }
 
         [HttpGet("get/{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            return Ok();
+            try
+            {
+                var sprint = await sprintManager.GetByIdAsync(id);
+                return Ok(sprint);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("get/board/{boardId}")]
         public async Task<IActionResult> GetByBoardId(int boardId)
         {
-            return Ok();
+            try
+            {
+                var sprints = await sprintManager.GetByBoardIdAsync(boardId);
+                return Ok(sprints);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost("create")]
         public async Task<IActionResult> Create(SprintModel model)
         {
-            return Ok();
-        }
-
-        [HttpPost("update-period")]
-        public async Task<IActionResult> UpdatePeriod(UpdatePeriodModel model)
-        {
-            return Ok();
+            try
+            {
+                var result = await sprintManager.CreateAsync(model);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost("update")]
         public async Task<IActionResult> Update(SprintModel model)
         {
-            return Ok();
+            try
+            {
+                var result = await sprintManager.UpdateAsync(model);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
