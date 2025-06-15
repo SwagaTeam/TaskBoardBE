20d60df941846d724c9f407be9f3fb187024f786
Данил <younggogy@gmail.com>
2025-06-15 21:06:06 +0500

    забыл допушить)

--- Shared/SharedLibrary/Models/UserModels/UserDtoModel.cs
+++ Shared/SharedLibrary/Models/UserModels/UserDtoModel.cs
@@ -2,5 +2,8 @@ namespace SharedLibrary.UserModels;
 
 public class UserDtoModel
 {
-    
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string ImagePath { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
