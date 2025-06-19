77fcd2e15b5a90fad6310f121cf63cf5e2509d32
Якшибаев Данил <younggogy@gmail.com>
2025-06-20 01:07:45 +0500

    change to https send invite message

--- Microservices/ProjectService/Controllers/ProjectController.cs
+++ Microservices/ProjectService/Controllers/ProjectController.cs
@@ -73,7 +73,7 @@ public class ProjectController : ControllerBase
         {
             var link = await _projectLinkManager.CreateAsync(request.ProjectId);
 
-            link = $"{Request.Scheme}://boardly.ru/project/invite/{link}";
+            link = $"https://boardly.ru/project/invite/{link}";
 
             var project = await _projectManager.GetByIdAsync(request.ProjectId);
 
