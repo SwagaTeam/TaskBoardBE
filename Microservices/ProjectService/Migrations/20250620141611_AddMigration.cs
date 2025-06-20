7849a6ebaf074ddbcf82a94dd8827092f6a7e46c
Якшибаев Данил <younggogy@gmail.com>
2025-06-20 19:16:36 +0500

    add migration

---
+++ Microservices/ProjectService/Migrations/20250620141611_AddMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "VisibilityLinks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "VisibilityLinks");
+        }
+    }
+}
