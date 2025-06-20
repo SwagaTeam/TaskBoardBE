7d0ee279663ae18924cd7c433eae55a64baf0afc
Якшибаев Данил <younggogy@gmail.com>
2025-06-20 21:38:56 +0500

    add migration

---
+++ Microservices/ProjectService/Migrations/20250620163846_addMigr.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectService.Migrations
+{
+    /// <inheritdoc />
+    public partial class addMigr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserProjects_Projects_ProjectId",
+                table: "UserProjects");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserProjects_Projects_ProjectId",
+                table: "UserProjects",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserProjects_Projects_ProjectId",
+                table: "UserProjects");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserProjects_Projects_ProjectId",
+                table: "UserProjects",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
